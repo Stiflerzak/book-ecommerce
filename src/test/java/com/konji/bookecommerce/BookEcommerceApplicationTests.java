67831a19de4f79fc087fc7bddecd76e4be9b0c2a
Stiflerzak <hilarymwendia97@gmail.com>
2023-06-21 11:48:58 +0300

    initial commit

---
+++ src/test/java/com/konji/bookecommerce/BookEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konji.bookecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
